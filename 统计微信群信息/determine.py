2e1ddd6323a507e9fd04ef165bdd9b16d26bb4b1
Grizzly <jayceelee@foxmail.com>
2018-12-27 21:01:11 +0800

    数字过滤

--- 统计微信群信息/determine.py
+++ 统计微信群信息/determine.py
@@ -9,7 +9,7 @@ class Info(object):
     @property
     def dealInfo(self, String):
         # pass
-        '''  
+        '''
         去除多于符号，后续分类处理文字和数字
         '''
         try:
@@ -20,7 +20,7 @@ class Info(object):
 
     @dealInfo.setter
     def dealInfo(self, value):
-        '''  
+        '''
         判断去除非数字文字的符号后判断是否有属性
         '''
         if value == None:
@@ -30,8 +30,8 @@ class Info(object):
 
     # @classmethod
     def getNumber(self):
-        '''  
-        过滤剩下文字，并且剩下排序返回数组
+        '''
+        过滤剩下数字，并且剩下排序返回数组
         '''
         # pass
         m = self.dealInfo(self.content)
@@ -43,10 +43,9 @@ class Info(object):
                 new.remove(' ')
         return new
 
-    
     def getLocation(self):
-        ''' 
-        处理文字部分，返回文字数组   
+        '''
+        处理文字部分，返回文字数组
         '''
         # pass
         m = self.dealInfo(self.content)
@@ -54,25 +53,86 @@ class Info(object):
             num = re.sub("\d", " ", m)
             c = num.strip()
             word = jieba.__lcut(c)
-            """  
+            """
             待处理一些图文符号
             """
 
             while ' ' in word:
                 word.remove(' ')
         return word
-    
+
     def sortNum(self, num):
-        """  
+        """
         处理剩下数字的结果，并且排序，无值设0
         功能:
-            97年 28  ，识别判断输出年龄
+            182 97年 28  ，识别判断输出年龄和身高
+            判断“92快93”这儿类字眼，以
         ------
-        age | stature
+        stature | age
 
         """
-        pass
-    
+        # pass
+        newNum = []
+        if isinstance(num, list):
+            Sort = sorted(num, reverse=True)
+            if 0 <= len(num) <= 2:
+               if len(num) == 0:
+                    newNum.extend([0, 0])
+                elif len(num) == 1:
+                    """ 判断第一个数是哪一个 """
+                    if num[0] > 100:
+                        num.extend([0])
+                        return num
+                    elif 80 < num[0] < 100:
+                        num.insert(0, 0)
+                        return num
+                    elif 10 < num[0] < 30:
+                        num.insert(0, 0)
+                        return num
+                    else:
+                        raise ValueError("出错")
+                elif len(num) == 2:
+                    if num[0] > 100:
+                        if 80 < num[1] < 100:
+                            return num
+                        elif 10 < num[1] < 30:
+                            return num
+                    elif 80 < num[0] < 100:
+                        """ 过滤两个90 """
+                        if 80 < num[1] < 100:
+                            num.pop()
+                            num.insert(0, 0)
+                            return num
+                        elif 10 < num[1] < 30:
+                            num.pop(0)
+                            num.extend([0])
+                            return num
+                        else:
+                            num.pop()
+                            num.insert(0, 0)
+                            return num
+                    elif 10 < num[0] < 30:
+                        """ 只有年龄 """
+                        num.pop()
+                        num.insert(0, 0)
+                        return num
+                else:
+                    raise ValueError("超过两个数字")
+            elif len(num) == 3:
+                num.pop()
+                self.sortNum(num)
+            else:
+                raise ValueError("输入不对")
+        return num
+
+        # elif num[0] > 100:
+        #     newNum.append(num[0])
+        # else:
+        #     if 70 < num[0] < 100:
+        #         newNum.append(num[0])  """ 生日年份 """
+        #     else:
+        #         newNum.append([num[0]]) """ 年龄 """
+
     def sortWord(self, word):
         """  
         处理剩下文字的结果，并排序列,
@@ -94,6 +154,3 @@ class Info(object):
         number = self.getNumber()
         Word = self.getLocation()
         pass
-    
-
-
---
+++ 统计微信群信息/test/test03.py
@@ -0,0 +1,14 @@
+num = [1,2,3,4]
+if isinstance(num, list):
+    # print("ok")
+    print(sorted(num, reverse=True))
+
+numa = []
+print(len(numa))
+
+num.insert(0,0)
+print(num)
+# for i in num:
+#     print(i)
+num.pop(0)
+print(num)
