4b1f5f502d11ed22fe2dd9e83f21eb879adbfced
Grizzly <jayceelee@foxmail.com>
2018-12-31 12:35:32 +0800

    测试

--- StatisticsWechat/test/test_determin.py
+++ StatisticsWechat/test/test_determin.py
@@ -1,13 +1,16 @@
 from StatisticsWechat.determine import Info
 import pytest
 
+
 def test_Info_sortNum():
-    test_info = Info([182,96,1]).sortNum([90,183,23])
+    test_info = Info([182, 96, 1]).sortNum([90, 183, 23])
     assert type(test_info) == list
     assert len(test_info) == 2
     assert test_info[0] == 183
-    assert test_info == [183,90]
+    assert test_info == [183, 90]
     # print(test_info)
+
+
 def test_Info_getLocation():
     test_get_number = Info(["男", "-", 182, 95]).getNumber()
-    assert test_get_number == [182,95]
+    assert test_get_number == [182, 95]
